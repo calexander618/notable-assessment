1694fa9ba729d3bc1ada1a0374a53c1fb1f9eaba
calexander618 <calexander618@gmail.com>
2023-01-07 13:46:57 -0700

    init

--- src/main/java/org/solution/TextProcessor.java
+++ src/main/java/org/solution/TextProcessor.java
@@ -1,17 +1,18 @@
 package org.solution;
 
 public class TextProcessor {
+
     public static void main(String[] args) {
-        String actual = transform("wtf");
+        String actual = transform("");
         System.out.print(actual);
     }
 
     public static String transform(String input) {
-        // bad cases (null, empty, etc)
+        // handle edge cases (null, empty, etc)
 
         StringBuilder builder = new StringBuilder();
 
-        for (int i=0; i<input.length(); i++) {
+        for (int i=0; i < input.length(); i++) {
             builder.append(input.charAt(i));
         }
 
